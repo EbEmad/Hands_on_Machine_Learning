9aca0a68c0994a76546cd54593f82138fac6771b
Ebrahim Emad <ebemad8@gmail.com>
2024-04-25 21:59:36 +0200

    adding logistic regression function

--- Linear_Regression/LinearRegression.py
+++ Linear_Regression/LinearRegression.py
@@ -1,24 +1,22 @@
 import numpy as np
+
 class LinearRegression:
-    def __init__(self,lr=.001,n_iter=1000):
+    def __init__(self,lr=0.001,n_iters=1000):
         self.lr=lr
-        self.n_iter=n_iter
-        self.weights=None
+        self.n_iters=n_iters
+        self.wieghts=None
         self.bias=None
-
     def fit(self,x,y):
-        n_samples,n_features = x.shape
-        #initialze weights and bias
-        self.weights=np.random.rand(n_features,1)
+        n_samples,n_features=x.shape
+        self.wieghts=np.zeros(n_features)
         self.bias=0
-        for _ in range(self.n_iter):
-            y_predicted = np.dot(x, self.weights).flatten() + self.bias
+        for _ in range(self.n_iters):
+            y_pred=np.dot(x,self.wieghts)+self.bias
+            dw=(1/n_samples)*np.dot(x.T,y_pred-y)
+            db=(1/n_samples)*np.sum(y_pred-y)
+            self.wieghts=self.wieghts-self.lr*dw
+            self.bias=self.bias-self.lr*db
 
-            #compute gradients
-            dw=(1/n_samples)*np.dot(x.T,(y_predicted-y).reshape(-1,1))
-            db=(1/n_samples)*np.sum(y_predicted-y)
-            self.weights-=self.lr*dw
-            self.bias-=self.lr*db
     def predict(self,x):
-        y_predicted=np.dot(x,self.weights).flaten()+self.bias
-        return y_predicted
+        y_pred=np.dot(x,self.wieghts)+self.bias
+        return y_pred
--- Linear_Regression/train.py
+++ Linear_Regression/train.py
@@ -5,22 +5,21 @@ import matplotlib.pyplot as plt
 from LinearRegression import LinearRegression 
 x,y=datasets.make_regression(n_samples=100,n_features=1,noise=20,random_state=4)
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=1234)
-reg=LinearRegression(lr=0.01)
+fig=plt.figure(figsize=(8,6))
+plt.scatter(x[:,0],y,color="b",marker='o',s=30)
+plt.show()
+reg=LinearRegression(0.01) # note the difference if u change the lr
 reg.fit(x_train,y_train)
 predictions=reg.predict(x_test)
 def mse(y_test,predictions):
     return np.mean((y_test-predictions)**2)
 mse=mse(y_test,predictions)
-print(f'MSE : {mse}')
+print(mse)
 y_pred_line=reg.predict(x)
 cmp=plt.get_cmap('viridis')
-fig=plt.figure(figsize=(0,6))
-m1 = plt.scatter(x_train, y_train, c='blue', s=10, label='Trained points')
-
-m2 = plt.scatter(x_test, y_test, c='red', s=10, label='Test points')
-
-plt.plot(x,y_pred_line,color='black',linewidth=2,label='fitted line')
-plt.legend()
+fig=plt.figure(figsize=(8,6))
+m1=plt.scatter(x_train,y_train,color=cmp(0.9),s=10)
+m2=plt.scatter(x_test,y_test,color=cmp(0.5),s=10)
+plt.plot(x,y_pred_line,color='black',linewidth=2,label='predictions')
 plt.show()
-
 
